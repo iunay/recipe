f4495e0f2c2dc324605321f694ad3894a53b5f24
iunay <ilary.poggi@gmail.com>
2020-12-08 13:14:57 +0100

    added manyToMany relationship Recipe - Category, setup config h2 db

--- src/main/java/com/demo/recipe/domain/Recipe.java
+++ src/main/java/com/demo/recipe/domain/Recipe.java
@@ -24,12 +24,15 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
-
 
     public Long getId() {
         return Id;
@@ -111,6 +114,13 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
